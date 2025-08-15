beede138b03b187e4c39554fc9ef6c7d12caeb59
Antonio Gabriel <nascimentogabriel.2004@gmail.com>
2025-08-15 13:13:50 -0300

    0.0.1

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*
+1. Cadastrar Item: Permite adicionar novos produtos ao menu do restaurante.
+2. Atualizar Item: Possibilita modificar informações de itens já cadastrados.
+3. Criar Pedido: Inicia o processo de montagem de um novo pedido.
+4. Atualizar Pedido: Permite alterar o status de um pedido existente.
+5. Consultar Pedidos: Exibe informações sobre os pedidos, com a opção de filtrar por status.
+*/
+
 fun main() {
-    print("Hello World!")
+    while(true) {
+        println("[1] Cadastrar Item: ")
+        println("[2] Atualizar Item: ")
+        println("[3] Criar Pedido: ")
+        println("[4] Atualizar Pedido: ")
+        println("[5] Consultar Pedidos: ")
+        print("Qual opção deseja escolher: ")
+
+        var entrada = readln().toInt()
+
+        when (entrada) {
+            1 -> {
+                println("Cadastrar Item")
+            }
+            2 -> {
+                println("Atualizar Item")
+            }
+            3 -> {
+                println("Criar Pedido")
+            }
+            4 -> {
+                println("Atualizar Pedido")
+            }
+            5 -> {
+                println("Consultar Pedidos")
+            }
+        }
+    }
 }
